9c9251c20efd6f4cb04d6e02cd511e525489b3c6
Markus Felten <Markus.Felten@gmx.de>
2013-04-26 13:48:56 +0200

    optionally handle http OPTIONS request by returning status: 200

--- src/main/java/org/jvnet/hudson/plugins/cors/JenkinsCorsFilter.java
+++ src/main/java/org/jvnet/hudson/plugins/cors/JenkinsCorsFilter.java
@@ -2,6 +2,7 @@ package org.jvnet.hudson.plugins.cors;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
@@ -12,6 +13,8 @@ import java.io.IOException;
 public class JenkinsCorsFilter implements Filter {
 
     private static final String CORS_ORIGIN = System.getProperty("cors.origin", "*");
+    private static final String CORS_HANDLE_OPTIONS_METHOD = System.getProperty("cors.options", "true");
+
     private static final String CORS_METHODS = System.getProperty("cors.methods", "GET, POST, PUT, DELETE");
     private static final String CORS_HEADERS = System.getProperty("cors.headers", "Authorization");
 
@@ -22,10 +25,18 @@ public class JenkinsCorsFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		if (response instanceof HttpServletResponse) {
+
             final HttpServletResponse resp = (HttpServletResponse) response;
             resp.addHeader("Access-Control-Allow-Origin", CORS_ORIGIN);
             resp.addHeader("Access-Control-Allow-Methods", CORS_METHODS);
             resp.addHeader("Access-Control-Allow-Headers", CORS_HEADERS);
+            if(request instanceof HttpServletRequest && Boolean.valueOf(CORS_HANDLE_OPTIONS_METHOD)) {
+                HttpServletRequest req = (HttpServletRequest)request;
+                if("OPTIONS".equals(req.getMethod())) {
+                    resp.setStatus(200);
+                   return;
+                }
+            }
         }
         chain.doFilter(request, response);
     }
